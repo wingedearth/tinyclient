f13c0901f5a47e37eaf6a91a8a77df952f1c3241
Andrew Anissi <aanissi@legacy.com>
2017-04-15 21:05:30 -0700

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-build
-coverage
-esdoc
-logs
-node_modules
-npm-debug.log
-reports
-src/external/build
-.idea
-.DS_Store
-*.sublime-workspace
-.nyc_output
-dist
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/client/app.js',
+  output: {
+    publicPath: `/`,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  target: 'node'
+};
