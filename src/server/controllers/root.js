a2fc76a204e9f422761d5dac708a2f156ea8b128
Andrew Anissi <aanissi@legacy.com>
2017-06-17 18:41:09 -0700

    send initialState from server to window

--- src/server/controllers/root.js
+++ src/server/controllers/root.js
@@ -1,4 +1,9 @@
-import _ from 'lodash';
+import {renderToString} from 'react-dom/server';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import React from 'react';
+import Home from '../../client/components/Home';
+import reducers from '../../client/redux/reducers';
 
 /**
  * Root controller for root router
@@ -7,8 +12,25 @@ import _ from 'lodash';
  * @returns {obj} response with homepage
  */
 export default function (req, res) {
-  const markup = '<p>Testing some markup data</p>';
-  const data = {markup};
+
+  const store = createStore(reducers);
+  const markup = renderToString(
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+  const initialState = {
+    todos: [{
+      id: 0,
+      completed: false,
+      text: 'Initial todo for demo purposes'
+    }],
+    user: {
+      username: 'Andrew',
+      id: 1
+    }
+  };
+  const data = {markup, initialState};
 
   return res.render('index', data);
 }
