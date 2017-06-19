1414a17994df1823b7536016a49548797039fdda
Andrew Anissi <aanissi@legacy.com>
2017-06-18 23:35:34 -0700

    check DOM before starting app. set up hot module reloading in app.js

--- src/client/app.js
+++ src/client/app.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Home from './components/Home';
-import buildStore from './redux/store';
+import configureStore from './redux/store';
 import {Provider} from 'react-redux';
 
-setTimeout(() => {
-  const initialState = window.__INITIAL_STATE__;
-  const store = buildStore(initialState);
+const mainElement = document.getElementById('main');
 
-  render(
+/**
+ * @param  {func} fn callback to run when DOM is ready
+ * @return {undefined}
+ */
+function whenDOMIsReady (fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', () => {
+      if (document.readyState !== 'loading') {
+        fn();
+      }
+    });
+  }
+}
+
+/**
+ * start app.js when DOM is loaded
+ * @returns {undefined}
+ */
+function startApp () {
+  const initialState = window.__INITIAL_STATE__;
+  const store = configureStore(initialState);
+
+  const renderElement = (
     <Provider store={store}>
       <Home />
-    </Provider>,
-    document.getElementById('main')
+    </Provider>
   );
-}, 10);
 
-if (module.hot) {
-  module.hot.accept();
+  render(renderElement, mainElement);
+  if (module.hot) {
+    module.hot.accept('./components/Home', () => {
+      render(
+        <Provider store={store}>
+            <Home />
+        </Provider>,
+        mainElement
+      );
+    });
+  }
 }
+
+// when the DOM is ready, Start the App!
+whenDOMIsReady(startApp);
