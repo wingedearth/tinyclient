fe2fd3f53a4a6dd6a0aebf7841b18450f32f4daa
Andrew Anissi <aanissi@legacy.com>
2017-06-18 23:12:06 -0700

    configure webpack with webpack-hot-middleware and react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const _ = require('lodash');
+const rules = require('./webpack/rules');
+const plugins = require('./webpack/plugins');
 
 const config = {
   devtool: 'inline-source-map',
   output: {
-    publicPath: '/',
+    publicPath: '/build',
     path: path.join(__dirname, 'build'),
     filename: '[name].js'
   },
@@ -17,34 +19,33 @@ const config = {
     ]
   },
   module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/
-      }
-    ]
+    rules
   }
 };
 
 const frontend = _.merge({}, config, {
+  name: 'frontend',
   entry: {
-    app: path.resolve(__dirname, 'src', 'client', 'app.js')
-  }
+    app: [
+      'react-hot-loader/patch',
+      'webpack-hot-middleware/client?name=frontend',
+      './src/client/app.js'
+    ]
+  },
+  plugins: plugins.frontend
 });
 
 const backend = _.merge({}, config, {
+  name: 'backend',
   entry: {
-    server: path.resolve(__dirname, 'src', 'server', 'server.js')
+    server: [
+      './src/server/server.js'
+    ]
   },
   output: {
     libraryTarget: 'commonjs2'
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
+  plugins: plugins.backend,
   target: 'node',
   node: {
     console: false,
